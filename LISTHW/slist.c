4b4ce62b811346bb866833303b99792076f6ffa2
cozt <42822504+cozt@users.noreply.github.com>
2020-04-10 13:41:09 -0700

    Add files via upload

---
+++ LISTHW/slist.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include<stdlib.h>
+#include "slist.h"
+#include "utils.h"
+
+snode* snode_create(int data, snode* next){
+  snode*p = (snode*)malloc(sizeof(snode));
+  p->data= data;
+  p->next = next;
+  return p;
+}
+slist* slist_create(void){
+  slist *p= (slist*)malloc(sizeof(slist));
+  p->head = NULL;
+  p->size = 0;
+  p->tail = NULL;
+  return p;
+}
+void checkempty(slist *list){
+  if(slist_empty(list)){
+    fprintf(stderr, "list is empty");
+    exit(1);
+  }
+}
+bool slist_empty(slist* list){
+return list->size==0;
+}
+size_t slist_size(slist* list){
+return list->size;
+}
+
+void slist_popfront(slist* list){
+
+snode*p = list->head;
+list->head= list->head->next;
+free(p);
+--list->size;
+}
+void slist_popback(slist* list){
+  if(slist_empty(list)){fprintf(stderr, "Underflow error \n");}
+
+  snode *p= list->head;
+  snode *pre;
+  while(p->next != NULL){pre =p; p = p->next;}
+  pre->next = NULL;
+  free(p);
+  --list->size;
+}
+
+
+void slist_pushfront(slist* list, int data){
+snode *p = snode_create(data,list->head);
+list->head=p;
+if(list->size==0){list->tail=p;}
+++list->size;
+}
+void slist_pushback(slist* list, int data){
+if(list->size==0){slist_pushfront(list,data); return;}
+snode*p=snode_create(data,NULL);
+list->tail->next=p;
+list->tail=p;
+++list->size;
+}
+
+void slist_clear(slist* list){
+while(!slist_empty(list)){
+  slist_popback(list);
+}
+}
+
+void slist_print(slist* list, const char* msg){
+printf("%s", msg);
+if(slist_empty(list)){printf("List is empty \n"); return; }
+snode *p= list->head;
+while(p!=NULL){
+  printf("%d --> %p\n",p->data,p->next);
+  p=p->next;
+}
+
+}
+
+int slist_front(slist* list){
+  checkempty(list);
+return list->head->data;
+}
+int slist_back(slist* list){
+  checkempty(list);
+return list->tail->data;
+}
